54bb2bf9f062a93e781fc1047ff3f5f7551b5636
simonscheider <simonscheider@web.de>
2015-11-04 15:29:03 +0100

    Update AlgebraReferenceSystems.thy

--- AlgebraReferenceSystems.thy
+++ AlgebraReferenceSystems.thy
@@ -5,7 +5,7 @@ begin
 declare [[show_types]]
 
 --{*
-An Isabelle theory describing spatio-temporal data generation. It is a supplement to the paper "A generative algebra for spatio-temporal information". 
+An Isabelle theory describing spatio-temporal data generation. It is a supplement to the paper Modelling spatio-temporal information generation". 
 The theory contains no proofs because all necessary reasoning is done on the type checking level.
 Authors: Simon Scheider, Benjamin Gräler, Christoph Stasch and Edzer Pebesma
 *}
